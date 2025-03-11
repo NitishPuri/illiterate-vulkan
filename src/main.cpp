023e59b0e516840deea787a29b0e56d4a90243a0
Nitish Puri <npuri1903@gmail.com>
2025-03-12 01:28:37 +0530

    vertex buffers+

--- src/main.cpp
+++ src/main.cpp
@@ -72,6 +72,11 @@ struct Vertex {
   }
 };
 
+const std::vector<Vertex> vertices =       //
+    {{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},  //
+     {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},   //
+     {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}};
+
 #pragma endregion VERTEX_DESC
 
 #pragma region VALIDATION_CALLBACK
@@ -168,6 +173,7 @@ class App {
     createGraphicsPipeline();
     createFrameBuffers();
     createCommandPool();
+    createVertexBuffer();
     createCommandBuffers();
     createSyncObjects();
   }
@@ -187,6 +193,9 @@ class App {
     LOGFN;
 
     cleanupSwapChain();
+
+    LOGCALL(vkDestroyBuffer(device, vertexBuffer, nullptr));
+    LOGCALL(vkFreeMemory(device, vertexBufferMemory, nullptr));
 
     LOGCALL(vkDestroyPipeline(device, graphicsPipeline, nullptr));
     LOGCALL(vkDestroyPipelineLayout(device, pipelineLayout, nullptr));
@@ -251,6 +260,9 @@ class App {
   std::vector<VkSemaphore> imageAvailableSemaphores;
   std::vector<VkSemaphore> renderFinishedSemaphores;
   std::vector<VkFence> inFlightFences;
+
+  VkBuffer vertexBuffer;
+  VkDeviceMemory vertexBufferMemory;
 
   bool framebbufferResized = false;
 
@@ -1042,7 +1054,57 @@ class App {
       }
     }
   }
+
 #pragma endregion FRAME_BUFFERS
+
+#pragma region VERTEX_BUFFERS
+  void createVertexBuffer() {
+    LOGFN;
+
+    VkBufferCreateInfo bufferInfo{};
+    bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+    bufferInfo.size = sizeof(vertices[0]) * vertices.size();
+    bufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+    bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+    if (LOGCALL(vkCreateBuffer(device, &bufferInfo, nullptr, &vertexBuffer)) != VK_SUCCESS) {
+      throw std::runtime_error("failed to create vertex buffer!");
+    }
+
+    // Memory requirements
+    LOG("Memory Requirements");
+    VkMemoryRequirements memRequirements;
+    vkGetBufferMemoryRequirements(device, vertexBuffer, &memRequirements);
+
+    VkMemoryAllocateInfo allocInfo{};
+    allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+    allocInfo.allocationSize = memRequirements.size;
+    allocInfo.memoryTypeIndex =
+        findMemoryType(memRequirements.memoryTypeBits,  //
+                       VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+
+    if (LOGCALL(vkAllocateMemory(device, &allocInfo, nullptr, &vertexBufferMemory)) != VK_SUCCESS) {
+      throw std::runtime_error("failed to allocate vertex buffer memory!");
+    }
+
+    LOGCALL(vkBindBufferMemory(device, vertexBuffer, vertexBufferMemory, 0));
+  }
+
+  uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties) {
+    LOGFN;
+    VkPhysicalDeviceMemoryProperties memProperties;
+    LOGCALL(vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties));
+
+    for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+      if ((typeFilter & (1 << i)) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {
+        return i;
+      }
+    }
+
+    throw std::runtime_error("failed to find suitable memory type!");
+  }
+
+#pragma endregion VERTEX_BUFFERS
 
 #pragma region COMMAND_BUFFERS
   void createCommandPool() {
